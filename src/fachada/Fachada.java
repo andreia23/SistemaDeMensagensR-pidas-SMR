2ade7edaa5869b14e8ff6ccd55205cd3997d16ce
andreia23 <44982439+andreia23@users.noreply.github.com>
2019-12-09 16:21:03 -0300

    Update Fachada.java

--- src/fachada/Fachada.java
+++ src/fachada/Fachada.java
@@ -1,6 +1,10 @@
 package fachada;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 import modelo.Pessoa;
+
 import repositorio.Repositorio;
 
 public class Fachada {
@@ -8,7 +12,7 @@ public class Fachada {
 	private static Pessoa logado;
 	private static int idprateleira=0;	//autoincremento
 
-	///localiza apessoa no repositório, a torna pessoa logada e retorna esta pessoa
+	///localiza apessoa no repositÃ³rio, a torna pessoa logada e retorna esta pessoa
 	
 	public static Pessoa login(String email, String senha) throws  Exception {
 		if(logado!=null)
@@ -36,4 +40,49 @@ public class Fachada {
 		logado = null; 
 		return usu;
 	}
+	
+	// torna a pesso a logada
+	
+	public static Pessoa getLogado() {
+		return logado;
+	}
+	
+	//cadastrar Pessoas
+	
+	public static Pessoa cadastrarPessoa(String email, String senha, String nome, BufferedImage iconimage) 
+		///FALTA
+		throws  Exception{
+			Pessoa usu = repositorio.localizarUsuario(email,senha);
+			if (usu!=null)
+				throw new Exception("cadastrar usuario - ja cadastrado:" + email);
+			
+			usu = new Pessoa(email,senha,nome);
+			repositorio.adicionar(usu);
+			return usu;
+	}
+	
+	///cadastrar Administrador
+	/*public static Pessoa cadastrarAdministrador(String email, String senha, String nome, BufferedImage iconimage,String setor) {
+		return "Falta";}*/
+	
+	public static ArrayList<Pessoa> listarPessoas() {
+		return repositorio.getPessoas();
+		
+	}
+	
+	/*public static ArrayList<Pessoa> listarPessoas(String termo) {
+		if (termo =  )
+		
+		return repositorio.getPessoas();
+		
+	}*/
+		
+	
+	
+	
+	
+	
+	
+	
+	
 }
