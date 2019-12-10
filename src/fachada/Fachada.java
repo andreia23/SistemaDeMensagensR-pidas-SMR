8984b2b83be92c9976911889f24176b5464220ea
andreia23 <44982439+andreia23@users.noreply.github.com>
2019-12-10 15:18:15 -0300

    Update Fachada.java

--- src/fachada/Fachada.java
+++ src/fachada/Fachada.java
@@ -61,30 +61,54 @@ public class Fachada {
 	///cadastrar Administrador
 	/*public static Pessoa cadastrarAdministrador(String email, String senha, String nome, BufferedImage iconimage,String setor) {
 		return "Falta";}*/
-	public static ArrayList<Pessoa> listarPessoas() {
-		return repositorio.getPessoas();
+	
+	
+	public static ArrayList<Pessoa> listarPessoas(String termo) {
+		return repositorio.getPessoas(termo);
 		
 	}
 	
-	//cria uma nova mensagem, considerando que o emitente � a pessoa logada
-	/*ic static Mensagem enviarMensagem(String destinatario, String texto) {
-			throw Exception{
+		
+	
+	//cria uma nova mensagem, considerando que o emitente é a pessoa logada
+	/*public static Mensagem enviarMensagem(String destinatario, String texto) {
+		throws Exception{
 				Pessoa usu = repositorio.setMensagem(mensagem);
-				if(usu != null)
+				if(usu==null) {					
+					throw new Exception("")
+				}
+				
 			}
 	}
+	*/
 	public static ArrayList<Mensagem> listarCaixaEntrada(){
 		return repositorio.getMensagens();
-	}*/
+	}
 	
-	/*public static ArrayList<Pessoa> listarPessoas(String termo) {
-		if (termo =  )
-		
-		return repositorio.getPessoas();
-		
-	}*/
 		
 	
+	//Listar Caixa de Entrada - arraylist
+	// Retorna as mensagem recebidas pela pessoa logada
+	
+	
+	//Listar Caixa de Saída - arraylist
+	// Retorna as mensagen enviadas pela pessoa logada
+	
+	
+	//Apagar Mensagem
+	// Exclui a mensagem da caixa de entrada e/ou saida da pessoa logada e retorna a mensagem excluida
+	
+	
+	//Espionar Mensagem - arraylist
+	//Retorna as mensagens cujo texto contem o termo fornecido
+	
+	
+	//Relatorio 01 - arraylist
+	//Retorna as pessoas que nao enviaram mensagem
+
+	
+	//Relatorio 02 - arraylist
+	//Retorna as mensagens com emitente igual ao destinatario
 	
 	
 	
